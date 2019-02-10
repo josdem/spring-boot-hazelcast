26f0afd59ba866b0f610baec3f556df03141b773
josdem <joseluis.delacruz@gmail.com>
2019-02-10 12:11:24 -0500

    Starting new spring webflux project using hazelcast :)

---
+++ src/main/java/com/jos/dem/springboot/hazelcast/HazelcastApplication.java
@@ -0,0 +1,14 @@
+package com.example.springboothazelcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastApplication {
+
+	public static void main(String[] args) {
+    SpringApplication.run(HazelcastApplication.class, args);
+	}
+
+}
+
