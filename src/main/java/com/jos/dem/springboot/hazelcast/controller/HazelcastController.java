83a9aafcc351883f0236d048b6daf289f897c2ef
josdem <joseluis.delacruz@gmail.com>
2019-02-10 15:11:52 -0500

    Adding hazelcast instance to store key and value

--- src/main/java/com/jos/dem/springboot/hazelcast/controller/HazelcastController.java
+++ src/main/java/com/jos/dem/springboot/hazelcast/controller/HazelcastController.java
@@ -1,10 +1,15 @@
 package com.jos.dem.springboot.hazelcast.controller;
 
+import java.util.Map;
+
+import com.hazelcast.core.HazelcastInstance;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,11 +18,16 @@ import org.slf4j.LoggerFactory;
 @RequestMapping("/hazelcast")
 public class HazelcastController {
 
+  @Autowired
+  private HazelcastInstance hazelcastInstance;
+
   private Logger log = LoggerFactory.getLogger(this.getClass());
 
   @PostMapping("/write/{key}/{value}")
   public String write(@PathVariable("key") String key, @PathVariable("value") String value) {
     log.info("Storing key: {} with value: {}", key, value);
+    Map<String, String> map = hazelcastInstance.getMap("memory");
+    map.put(key, value);
     return "Key and value stored";
   }
 
