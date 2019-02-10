1acedca6542766a318a1b86b556813caba79fc40
josdem <joseluis.delacruz@gmail.com>
2019-02-10 12:26:46 -0500

    Adding Hazelcast configuration

--- src/main/java/com/jos/dem/springboot/hazelcast/HazelcastApplication.java
+++ src/main/java/com/jos/dem/springboot/hazelcast/HazelcastApplication.java
@@ -1,4 +1,4 @@
-package com.example.springboothazelcast;
+package com.jos.dem.springboot.hazelcast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
