0061c2e96abeea8f3917a1c88611719aa6ec4236
josdem <joseluis.delacruz@gmail.com>
2019-02-10 12:47:31 -0500

    Adding request mapping to the controller

--- src/main/java/com/jos/dem/springboot/hazelcast/controller/HazelcastController.java
+++ src/main/java/com/jos/dem/springboot/hazelcast/controller/HazelcastController.java
@@ -3,19 +3,21 @@ package com.jos.dem.springboot.hazelcast.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RestController
+@RequestMapping("/hazelcast")
 public class HazelcastController {
 
   private Logger log = LoggerFactory.getLogger(this.getClass());
 
   @PostMapping("/write/{key}/{value}")
   public String write(@PathVariable("key") String key, @PathVariable("value") String value) {
-    log.info("Storing key: {0} with valye: {1}", key, value);
+    log.info("Storing key: {} with value: {}", key, value);
     return "Key and value stored";
   }
 
