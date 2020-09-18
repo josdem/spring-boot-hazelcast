c9e1b5cffdee791429e15744b4560a7544281483
josdem <joseluis.delacruz@gmail.com>
2020-09-17 21:15:05 -0400

    Avoiding wildcard on imports

--- src/main/java/com/jos/dem/springboot/hazelcast/controller/HazelcastController.java
+++ src/main/java/com/jos/dem/springboot/hazelcast/controller/HazelcastController.java
@@ -3,7 +3,11 @@ package com.jos.dem.springboot.hazelcast.controller;
 import com.hazelcast.core.HazelcastInstance;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -13,21 +17,20 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class HazelcastController {
 
-    private final HazelcastInstance hazelcastInstance;
+  private final HazelcastInstance hazelcastInstance;
 
-    @PostMapping("/write/{key}/{value}")
-    public String write(@PathVariable("key") String key, @PathVariable("value") String value) {
-        log.info("Storing key: {} with value: {}", key, value);
-        Map<String, String> map = hazelcastInstance.getMap("memory");
-        map.putIfAbsent(key, value);
-        return "Key and value stored";
-    }
+  @PostMapping("/write/{key}/{value}")
+  public String write(@PathVariable("key") String key, @PathVariable("value") String value) {
+    log.info("Storing key: {} with value: {}", key, value);
+    Map<String, String> map = hazelcastInstance.getMap("memory");
+    map.putIfAbsent(key, value);
+    return "Key and value stored";
+  }
 
-    @GetMapping("/read/{key}")
-    public String read(@PathVariable("key") String key) {
-        log.info("Reading stored value with key: {}", key);
-        Map<String, String> map = hazelcastInstance.getMap("memory");
-        return map.get(key);
-    }
-
+  @GetMapping("/read/{key}")
+  public String read(@PathVariable("key") String key) {
+    log.info("Reading stored value with key: {}", key);
+    Map<String, String> map = hazelcastInstance.getMap("memory");
+    return map.get(key);
+  }
 }
