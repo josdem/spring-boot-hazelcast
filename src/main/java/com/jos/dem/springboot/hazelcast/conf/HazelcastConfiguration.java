252e8d4e109d25a1c80ca89d3cfc85692b1cec8e
josdem <joseluis.delacruz@gmail.com>
2025-08-24 13:43:00 -0600

    Adding eviction config

--- src/main/java/com/jos/dem/springboot/hazelcast/conf/HazelcastConfiguration.java
+++ src/main/java/com/jos/dem/springboot/hazelcast/conf/HazelcastConfiguration.java
@@ -1,7 +1,10 @@
 package com.jos.dem.springboot.hazelcast.conf;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionConfig;
+import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.MaxSizePolicy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +15,16 @@ public class HazelcastConfiguration {
 
     @Bean
     public Config hazelCastConfig() {
+        var evictionConfig = new EvictionConfig()
+                .setEvictionPolicy(EvictionPolicy.LFU)
+                .setMaxSizePolicy(MaxSizePolicy.USED_HEAP_PERCENTAGE)
+                .setSize(MAX_SIZE);
+
         return new Config().setInstanceName("hazelcast-instance")
                 .addMapConfig(
                         new MapConfig()
                                 .setName("configuration")
+                                .setEvictionConfig(evictionConfig)
                                 .setTimeToLiveSeconds(-1));
     }
 
