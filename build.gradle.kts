7f926740f55f9444669527f3aed87266a38c8fd4
josdem <joseluis.delacruz@gmail.com>
2025-08-25 14:13:53 -0600

    Adding test

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.5"
 	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm")
 }
 
 group = "com.jos.dem.springboot.hazelcast"
@@ -31,6 +32,7 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<Test> {
---
+++ src/test/java/controller/HazelcastControllerTest.kt
@@ -0,0 +1,32 @@
+package controller
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInfo
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class HazelcastControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    @Test
+    fun `should store a value in the map`(testInfo: TestInfo) {
+        log.info(testInfo.displayName)
+        val request =
+            post("/hazelcast/write/1/value1")
+        mockMvc
+            .perform(request)
+            .andExpect(status().isOk())
+    }
+
+}
